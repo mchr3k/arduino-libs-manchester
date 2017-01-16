mchr3k/arduino-libs-manchester

commit 18d6cb83f1d07e32064cc99652a7fd60e00fa59a

Added support for ESP8266/Arduino

Manchester.cpp
@@ -82,6 +82,9 @@ void Manchester::setupTransmit(uint8_t pin, uint8_t SF)
     uint16_t compensationFactor = 4; 
   #endif  
 
+#if (F_CPU == 80000000UL) || (F_CPU == 160000000)   // ESP8266 80MHz or 160 MHz
+  delay1 = delay2 = (HALF_BIT_INTERVAL >> speedFactor) - 2;
+#else
   delay1 = (HALF_BIT_INTERVAL >> speedFactor) - compensationFactor;
   delay2 = (HALF_BIT_INTERVAL >> speedFactor) - 2;
   
@@ -93,7 +96,8 @@ void Manchester::setupTransmit(uint8_t pin, uint8_t SF)
       delay1 >>= 3;
       delay2 >>= 3;
     }
-  #endif  
+  #endif
+#endif
 }
 
 
@@ -260,12 +264,29 @@ void Manchester::stopReceive(void)
 
 //global functions
 
+#if defined( ESP8266 )
+   volatile uint16_t ESPtimer = 0;
+   void timer0_ISR (void);
+#endif
+
 void MANRX_SetupReceive(uint8_t speedFactor)
 {
   pinMode(RxPin, INPUT);
   //setup timers depending on the microcontroller used
 
-  #if defined( __AVR_ATtiny25__ ) || defined( __AVR_ATtiny45__ ) || defined( __AVR_ATtiny85__ )
+  #if defined( ESP8266 )
+   #if F_CPU == 80000000
+      ESPtimer = (512 >> speedFactor) * 80;  // 8MHZ, 300us for MAN_300, 128us for MAN_1200
+   #elif F_CPU == 160000000
+      ESPtimer = (512 >> speedFactor) * 160;
+   #endif
+
+   noInterrupts();
+   timer0_isr_init();
+   timer0_attachInterrupt(timer0_ISR);
+   timer0_write(ESP.getCycleCount() + ESPtimer); //80Mhz -> 128us
+   interrupts();
+  #elif defined( __AVR_ATtinyX5__ )
 
     /*
     Timer 1 is used with a ATtiny85. 
@@ -493,8 +514,9 @@ void AddManBit(uint16_t *manBits, uint8_t *numMB,
 }
 
 
-
-#if defined( __AVR_ATtiny25__ ) || defined( __AVR_ATtiny45__ ) || defined( __AVR_ATtiny85__ )
+#if defined( ESP8266 )
+void ICACHE_RAM_ATTR timer0_ISR (void)
+#elif defined( __AVR_ATtiny25__ ) || defined( __AVR_ATtiny45__ ) || defined( __AVR_ATtiny85__ )
 ISR(TIMER1_COMPA_vect)
 #elif defined( __AVR_ATtiny2313__ ) || defined( __AVR_ATtiny2313A__ ) || defined( __AVR_ATtiny4313__ )
 ISR(TIMER1_COMPB_vect)
@@ -618,6 +640,9 @@ ISR(TIMER2_COMPA_vect)
     // Get ready for next loop
     rx_last_sample = rx_sample;
   }
+#if defined( ESP8266 )
+  timer0_write(ESP.getCycleCount() + ESPtimer);
+#endif
 }
 
 Manchester man;

commit d6f78b611e8c8f293508a7094ce05fb16f5668b8

Fix ATtiny processor macro regression
__AVR_ATtinyX5__ is a nonstandard macro that may not be defined in all
hardware cores(i.e. damellis/attiny). This issue was fixed in
https://github.com/mchr3k/arduino-libs-manchester/commit/23880ca2556c4293e018cf272accb433753e8374
but partially  regressed in
https://github.com/mchr3k/arduino-libs-manchester/commit/18d6cb83f1d07e32064cc99652a7fd60e00fa59a.

Manchester.cpp
@@ -286,7 +286,7 @@ void MANRX_SetupReceive(uint8_t speedFactor)
    timer0_attachInterrupt(timer0_ISR);
    timer0_write(ESP.getCycleCount() + ESPtimer); //80Mhz -> 128us
    interrupts();
-  #elif defined( __AVR_ATtinyX5__ )
+  #elif defined( __AVR_ATtiny25__ ) || defined( __AVR_ATtiny45__ ) || defined( __AVR_ATtiny85__ )
 
     /*
     Timer 1 is used with a ATtiny85. 